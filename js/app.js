c2b062f1c7958dbd79221bf8b222b7b6a08b4a2b
cybertunnelofficial <85891685+cybertunnelofficial@users.noreply.github.com>
2021-07-18 12:31:58 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('../sw.js')
+    navigator.serviceWorker.register('https://cybertunnelofficial.github.io/progressive-web-app/sw.js')
         .then(reg => console.log('sw registered', reg))
         .catch(err => console.log('sw register err', err))
 }
